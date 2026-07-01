b3651fdf2902ad101a881f601b29840322ceb50b
behmanesham <Ibstrade11@gmail.com>
2026-07-01 20:54:27 +0330

    Add D2 evaluator tests

--- tests/evaluation/test_d2.py
+++ tests/evaluation/test_d2.py
@@ -1,2 +1,35 @@
-def test_d2_placeholder():
-    assert True
+from bsi_benchmark.evaluation.bsi import BSIEvaluator
+
+
+class Article:
+    def __init__(self, abstract=None, doi=None):
+        self.abstract = abstract
+        self.doi = doi
+        self.title = "Artificial Intelligence"
+
+
+class Dataset:
+    def __init__(self, articles):
+        self.articles = articles
+
+
+def test_d2_full():
+    score = BSIEvaluator().evaluate(
+        Dataset([
+            Article("a", "1"),
+            Article("b", "2"),
+            Article("c", "3"),
+        ])
+    )
+    assert score.d2 == 1.0
+
+
+def test_d2_partial():
+    score = BSIEvaluator().evaluate(
+        Dataset([
+            Article("a", "1"),
+            Article(None, "2"),
+            Article("c", None),
+        ])
+    )
+    assert abs(score.d2 - (1 / 3)) < 1e-9
