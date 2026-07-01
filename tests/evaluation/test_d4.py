7ff70b99f3f23a58d4855eebb3d215970b9e1b22
behmanesham <Ibstrade11@gmail.com>
2026-07-01 21:00:21 +0330

    Add D4 evaluator tests

--- tests/evaluation/test_d4.py
+++ tests/evaluation/test_d4.py
@@ -1,2 +1,23 @@
-def test_d4_placeholder():
-    assert True
+from bsi_benchmark.evaluation.bsi import BSIEvaluator
+
+
+class Article:
+    def __init__(self):
+        self.title = "Artificial Intelligence"
+        self.abstract = "abstract"
+        self.doi = "doi"
+
+
+class Dataset:
+    def __init__(self, n):
+        self.articles = [Article() for _ in range(n)]
+
+
+def test_d4_full():
+    score = BSIEvaluator().evaluate(Dataset(5))
+    assert score.d4 == 1.0
+
+
+def test_d4_partial():
+    score = BSIEvaluator().evaluate(Dataset(2))
+    assert score.d4 == 0.4
